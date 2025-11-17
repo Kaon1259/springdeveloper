06e18fae818f7a1a8c07a62f08ff05a59b5f9e13
Kaon1259 <kaon1259@naver.com>
2025-11-17 18:32:10 +0900

    UPDATE

--- src/main/java/ymsoft/springdeveloper/com/springdeveloper/controller/MainController.java
+++ src/main/java/ymsoft/springdeveloper/com/springdeveloper/controller/MainController.java
@@ -8,7 +8,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.support.SessionStatus;
 import ymsoft.springdeveloper.com.springdeveloper.dto.UserDto;
 import ymsoft.springdeveloper.com.springdeveloper.dto.UserLoginRequest;
 import ymsoft.springdeveloper.com.springdeveloper.entity.Users;
